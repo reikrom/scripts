2d0ffab7b7161bcdb0fa3c15627910deea9c9a38
reinis kromulis <reiniskromulis@Mac.localdomain>
2024-10-24 22:43:33 +0300

    feat: add hard pull script

---
+++ bash/git-hardPull.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# brew install parallel
+
+SEARCH_DIR="."
+
+find "$SEARCH_DIR" -mindepth 1 -maxdepth 1 -type d | \
+parallel -j 13 '
+    echo "Pulling updates in directory: {}" && \
+    (cd {} && git reset origin --hard && git pull)
+'
+
